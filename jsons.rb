a352ee3fdcb6edebe45ab9f3d5ff4fe59be6be2d
Hidenori <hfu@users.noreply.github.com>
2024-04-28 10:33:30 +0900

    update

--- jsons.rb
+++ jsons.rb
@@ -7,7 +7,7 @@ MINZOOM = {
   'aggregated_routes' => 8,
   'aggregated_stops' => 1,
   'routes' => 12,
-  'stops' => 10 
+  'stops' => 1 
 }
 $count = 0
 
@@ -29,12 +29,15 @@ end
 def process(url)
   Dir.mktmpdir {|dir|
     gtfs_path = "#{dir}/gtfs.zip"
-    geojson_path = "#{dir}/geojson"
+    parse_dir = "#{dir}/parse"
+    #aggregate_dir = "#{dir}/aggregate"
     command("curl --silent -o #{gtfs_path} #{url}")
-    command("#{PARSER} parse #{gtfs_path} #{geojson_path}")
-    command("#{PARSER} aggregate #{gtfs_path} #{geojson_path}")
-    %w{routes aggregated_routes stops aggregated_stops}.each {|t|
-      IO.popen("tippecanoe-json-tool #{geojson_path}/#{t}.geojson") {|io|
+    command("#{PARSER} parse #{gtfs_path} #{parse_dir}")
+    #command("#{PARSER} aggregate #{gtfs_path} #{aggregate_dir}")
+    #%w{routes aggregated_routes stops aggregated_stops}.each {|t|
+    %w{routes stops}.each {|t|
+      src_dir = /aggregated/.match(t) ? aggregate_dir : parse_dir
+      IO.popen("tippecanoe-json-tool #{src_dir}/#{t}.geojson") {|io|
         io.each_line {|l|
           feature(JSON.parse(l), t)
         }
